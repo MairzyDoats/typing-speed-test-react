2f34aedcf58dae3374350bde33f35b9a923a79ae
MairzyDoats <jk101@gmx.de>
2021-11-16 21:38:04 +0100

    Add header and logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 
-export default function Header() {
+export default function Header({ logo }) {
   return (
-    <header>
-
+    <header className="header">
+      <img className="header__logo" src={logo} alt="ready set type logo" />
     </header>
   )
 }
