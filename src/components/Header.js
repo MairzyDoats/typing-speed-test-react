e74f7e6cff2732290975e91b9c90ce283f63a2a2
MairzyDoats <jk101@gmx.de>
2021-11-17 13:07:26 +0100

    add h1

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ export default function Header({ logo }) {
   return (
     <header className="header">
       <img className="header__logo" src={logo} alt="ready set type logo" />
+      <h1 className="header__heading">Typing Speed Test</h1>
     </header>
   )
 }
