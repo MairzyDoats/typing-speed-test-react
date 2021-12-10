279139a9a8f942e5da478626076e2dc313e8a3ad
MairzyDoats <jk101@gmx.de>
2021-12-10 17:02:25 +0100

    Add responsive header

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import Main from './components/Main';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import logo from './ready-set-type.png';
+import alternateLogo from './ready-set-type-alternate.png';
 import './css/App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header logo={logo} />
+      <Header logo={logo} alternateLogo={alternateLogo} />
       <Main />
       <Footer />
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-export default function Header({ logo }) {
+export default function Header({ logo, alternateLogo }) {
+  const [width, setWidth] = useState(window.innerWidth)
+
+  useEffect(() => {
+
+    const updateDimensions = () => {
+      setWidth(window.innerWidth)
+    }
+
+    updateDimensions();
+    window.addEventListener("resize", updateDimensions);
+
+    return () =>
+      window.removeEventListener("resize", updateDimensions);
+  }, [])
+
+
   return (
     <header className="header">
-      <img className="header__logo" src={logo} alt="ready set type logo" />
+      <img className="header__logo" src={width > 600 ? logo : alternateLogo} alt="ready set type logo" />
       <h1 className="header__heading">Typing Speed Test</h1>
     </header>
   )
