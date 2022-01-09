4ff800d73595986392a12fbbb9d536954cf06e54
MairzyDoats <jk101@gmx.de>
2022-01-09 14:46:15 +0100

    add link to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   const year = new Date().getFullYear()
   return (
     <footer className="footer">
-      <p className="footer__copyright">©{year} Julian Krüger</p>
+      <p className="footer__copyright">©{year} <a className="footer__link" href="https://www.iamjulian.de">Julian Krüger</a></p>
     </footer>
   )
 }
