8f61f01dfa9eb58c30dfb4becc42ba393de955a8
MairzyDoats <jk101@gmx.de>
2022-01-09 15:01:15 +0100

    make timer more accurate

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -4,20 +4,33 @@ import { GameContext } from './Main';
 export default function Timer() {
   const { countWords, start, TIME } = useContext(GameContext);
   const [countdown, setCountdown] = useState(TIME);
+  const [startTime, setStartTime] = useState();
 
 
   useEffect(() => {
+    if (!start) return
+
+    const currentStartTime = new Date()
+    setStartTime(currentStartTime)
+  }, [start])
+
+  useEffect(() => {
+
+    const getTimerTime = () => {
+      return Math.floor((new Date() - startTime) / 1000)
+    }
+
     if (!start) {
       setCountdown(TIME);
     }
     if (countdown > 0 && start) {
-      var countDownTimeout = setTimeout(() => setCountdown((t) => t - 1), 1000);
+      var countDownTimeout = setTimeout(() => setCountdown(TIME - getTimerTime()), 1000);
     } else if (countdown === 0) {
       countWords();
       setCountdown(TIME);
     }
     return () => clearTimeout(countDownTimeout);
-  }, [start, countdown, countWords, TIME])
+  }, [start, countdown, countWords, TIME, startTime])
 
   return (
     <div className="data-section__dataset">
